0522023cf185f10aa12c0f6c95642c7b07366ff4
Casper <draakie@protonmail.com>
2023-08-30 15:41:56 +0200

    nvim: Improve completion bindings.

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -2,6 +2,8 @@
 -- Completion config
 --------------------------------------------------------------------------------
 
+local TableUtils = reload('utils.table')
+
 local function has_words_before()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
@@ -14,14 +16,17 @@ vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
-        -- ['<C-k>'] = cmp.mapping.scroll_docs(-4),
-        -- ['<C-j>'] = cmp.mapping.scroll_docs(4),
-        -- ['<C-Space>'] = cmp.mapping.complete(),
-        -- ['<C-x>'] = cmp.mapping.abort(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
 
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_next_item()
+                local count = TableUtils.count(cmp.get_entries())
+                if count == 1 then
+                    cmp.confirm({ select = false })
+                else
+                    cmp.select_next_item()
+                end
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
             elseif has_words_before() then
@@ -31,21 +36,26 @@ cmp.setup({
             end
         end, { 'i', 's' }),
 
-        -- TODO: Fix the shift-tab mapping not working.
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        ['<CR>'] = cmp.mapping(function(fallback)
+            local entry = cmp.get_selected_entry()
+            dump_table(entry)
             if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+                cmp.confirm({ select = false })
             else
                 fallback()
             end
         end, { 'i', 's' }),
 
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<Right>'] = cmp.mapping.confirm({ select = false }),
 
-        -- NOTE: Inlcuded for consistency with fish shell. Ideally we want to change fish shell completion bindings.
-        ['<Right>'] = cmp.mapping.confirm({ select = true }),
+        ['<Esc>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.abort()
+                vim.cmd.stopinsert()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
     }),
 
     completion = {
--- lua/utils/table.lua
+++ lua/utils/table.lua
@@ -14,5 +14,15 @@ function TableUtils.contains(table, value)
     return false
 end
 
+function TableUtils.count(table)
+    local count = 0
+
+    for _ in pairs(table) do
+        count = count + 1
+    end
+
+    return count
+end
+
 return TableUtils
 
