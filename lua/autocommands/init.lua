48e9f81307d832b95ab5b18b20892a1665e3480b
Casper <draakie@proton.me>
2024-06-02 15:27:07 +0200

    nvim: Adjust help buffer layout dynamically.

--- lua/autocommands/init.lua
+++ lua/autocommands/init.lua
@@ -3,7 +3,7 @@
 --------------------------------------------------------------------------------
 
 reload('autocommands.equalize_splits')
-reload('autocommands.open_help_vertically')
+reload('autocommands.layout_help')
 reload('autocommands.reload_config')
 
 -- NOTE: Not working as desired.
