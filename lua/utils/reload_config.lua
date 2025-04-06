5c53d2f831f55753a81920093d9dd0e4799a275d
Casper <draakie@proton.me>
2025-04-06 17:03:45 +0200

    chore: Minor clean up.

--- lua/utils/reload_config.lua
+++ lua/utils/reload_config.lua
@@ -11,9 +11,9 @@ function _G.reload_config()
     reload('editor.options')
     reload('editor.highlights')
 
-    -- TODO: Restore plugin reloading; requires a different plugin manager.
-    -- Load plugins. Will bootstrap Lazy.nvim if necessary.
-    -- reload('plugins.manager')
+    -- TODO: Restore plugin reloading; requires a different plugin manager,
+    -- LazyVim does not support this.
+    -- reload('plugins')
 
     -- Load autocommands.
     reload('autocommands')
