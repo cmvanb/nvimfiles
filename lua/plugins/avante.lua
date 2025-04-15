6143e1769f09fcb84df1a82a69f53d72f970443f
Casper <draakie@proton.me>
2025-04-15 18:54:40 +0200

    plugins: Add avante AI plugin.

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,32 @@
+--------------------------------------------------------------------------------
+-- Avante AI plugin
+--------------------------------------------------------------------------------
+
+-- Highlights
+local HighlightsUtils = reload('utils.highlights')
+local hi = HighlightsUtils.highlight
+local ln = HighlightsUtils.link
+
+hi('AvanteConflictCurrent',   { bg = 'red_1' },    { bg = 'ansi_red' })
+hi('AvanteConflictIncoming',  { bg = 'green_1' },  { bg = 'ansi_green' })
+
+-- Config
+return {
+    {
+        'yetone/avante.nvim',
+        event = 'VeryLazy',
+        -- Never set this value to '*'! Never!
+        version = false,
+        opts = {
+            provider = 'claude',
+        },
+        build = 'make',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'stevearc/dressing.nvim',
+            'nvim-lua/plenary.nvim',
+            'MunifTanjim/nui.nvim',
+            'MeanderingProgrammer/render-markdown.nvim',
+        },
+    }
+}
