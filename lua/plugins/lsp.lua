b766bfb1bf44b8657116689965a3faddee99002f
Casper <draakie@proton.me>
2025-05-13 01:40:33 +0200

    lsp: Use native 0.11.0 LSP config API.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,9 +4,12 @@
 -- Based on: https://github.com/VonHeikemen/lsp-zero.nvim/wiki/Under-the-hood
 --------------------------------------------------------------------------------
 
-local function mason_config()
-    local ThemeConfig = reload('theme.config')
+local ThemeConfig = reload('theme.config')
+local ThemeSymbols = reload('theme.symbols')
+local KeyMapUtils = reload('utils.keymaps')
+local TableUtils = reload('utils.table')
 
+local function mason_config()
     reload('mason').setup({
         ui = {
             border = ThemeConfig.border,
@@ -14,40 +17,34 @@ local function mason_config()
     })
 end
 
-local function nvim_lspconfig_config()
-    -- Imports
-    local KeyMapUtils = reload('utils.keymaps')
-    local TableUtils = reload('utils.table')
-    local ThemeConfig = reload('theme.config')
-    local ThemeSymbols = reload('theme.symbols')
-
-    -- Diagnostics UI
+local function lsp_config()
+    -- LSP generic on_attach configuration
     ----------------------------------------------------------------------------
-    vim.diagnostic.config({
-        virtual_text = false,
-        signs = {
-            text = {
-                [vim.diagnostic.severity.ERROR] = ThemeSymbols.diagnostic_signs.error,
-                [vim.diagnostic.severity.WARN] = ThemeSymbols.diagnostic_signs.warn,
-                [vim.diagnostic.severity.INFO] = ThemeSymbols.diagnostic_signs.info,
-                [vim.diagnostic.severity.HINT] = ThemeSymbols.diagnostic_signs.hint,
+    local function lsp_diagnostics()
+        vim.diagnostic.config({
+            virtual_text = false,
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = ThemeSymbols.diagnostic_signs.error,
+                    [vim.diagnostic.severity.WARN] = ThemeSymbols.diagnostic_signs.warn,
+                    [vim.diagnostic.severity.INFO] = ThemeSymbols.diagnostic_signs.info,
+                    [vim.diagnostic.severity.HINT] = ThemeSymbols.diagnostic_signs.hint,
+                },
             },
-        },
-        update_in_insert = false,
-        underline = true,
-        severity_sort = true,
-        float = {
-            focusable = false,
-            style = 'minimal',
-            border = ThemeConfig.border,
-            source = 'always',
-            header = '',
-            prefix = '',
-        },
-    })
+            update_in_insert = false,
+            underline = true,
+            severity_sort = true,
+            float = {
+                focusable = false,
+                style = 'minimal',
+                border = ThemeConfig.border,
+                source = 'always',
+                header = '',
+                prefix = '',
+            },
+        })
+    end
 
-    -- LSP functions
-    ----------------------------------------------------------------------------
     local function lsp_keymaps(bufnr)
         local opts = { silent = true, buffer = bufnr }
 
@@ -87,22 +84,19 @@ local function nvim_lspconfig_config()
     end
 
     local function lsp_attach(_, bufnr)
-        local buf_command = vim.api.nvim_buf_create_user_command
-
+        lsp_diagnostics()
         lsp_keymaps(bufnr)
 
-        buf_command(bufnr, 'LspFormat', function()
+        vim.api.nvim_buf_create_user_command(bufnr, 'LspFormat', function()
             vim.lsp.buf.format()
         end, { desc = 'Format buffer with language server' })
     end
 
-    -- LSP capabilities
+    -- LSP generic capabilities
     ----------------------------------------------------------------------------
-    local base_capabilities =
-        reload('blink.cmp').get_lsp_capabilities()
-
     local lsp_capabilities =
-        TableUtils.merge(base_capabilities, {
+        -- Enable blink.cmp and merge default capabilities
+        reload('blink.cmp').get_lsp_capabilities({
             textDocument = {
                 -- Enable snippets with LuaSnip.
                 completion = {
@@ -110,6 +104,7 @@ local function nvim_lspconfig_config()
                         snippetSupport = true,
                     },
                 },
+                -- TODO: Investigate -> We don't use UFO anymore, does this do anything useful?
                 -- Enable folding with UFO.
                 foldingRange = {
                     dynamicRegistration = false,
@@ -118,56 +113,94 @@ local function nvim_lspconfig_config()
             },
         })
 
-    local default_ls_config = {
+    -- LSP generic configuration
+    ----------------------------------------------------------------------------
+    reload('lspconfig')
+
+    vim.lsp.config('*', {
         on_attach = lsp_attach,
         capabilities = lsp_capabilities,
-    }
+    })
 
-    -- LSP server configuration
+    vim.lsp.enable({
+        'bashls',
+        'clangd',
+        'cssls',
+        'dockerls',
+        'html',
+        'jsonls',
+        'lua_ls',
+        'pyright',
+        'rust_analyzer',
+        'ts_ls',
+        'yamlls',
+    })
+
+    -- LSP specific configuration
     ----------------------------------------------------------------------------
-    local lspconfig = reload('lspconfig')
 
-    -- Lua
-    lspconfig.lua_ls.setup(
-        TableUtils.merge(default_ls_config, {
-            on_init = function(client)
+    --- Lua
+    vim.lsp.config('lua_ls', {
+        on_init = function(client)
+            if client.workspace_folders then
                 local path = client.workspace_folders[1].name
-                if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-                    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                        runtime = {
-                            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                            version = 'LuaJIT'
-                        },
-                        workspace = {
-                            -- Make the server aware of Neovim runtime files
-                            library = { vim.env.VIMRUNTIME }
-                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                            -- library = vim.api.nvim_get_runtime_file("", true)
-                        }
-                    })
-
-                    client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                if
+                    path ~= vim.fn.stdpath('config')
+                    and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+                then
+                    return
                 end
-                return true
-            end,
+            end
 
-            settings = {
-                Lua = {
-                    telemetry = {
-                        enable = false,
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using (most
+                    -- likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                    -- Tell the language server how to find Lua modules same way as Neovim
+                    -- (see `:h lua-module-load`)
+                    path = {
+                        'lua/?.lua',
+                        'lua/?/init.lua',
                     },
                 },
-            },
-        })
-    )
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                    checkThirdParty = false,
+                    library = {
+                        vim.env.VIMRUNTIME
+                        -- Depending on the usage, you might want to add additional paths
+                        -- here.
+                        -- '${3rd}/luv/library'
+                        -- '${3rd}/busted/library'
+                    }
+                    -- Or pull in all of 'runtimepath'.
+                    -- NOTE: this is a lot slower and will cause issues when working on
+                    -- your own configuration.
+                    -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+                    -- library = {
+                    --   vim.api.nvim_get_runtime_file('', true),
+                    -- }
+                }
+            })
+
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        end,
+
+        settings = {
+            Lua = {
+                telemetry = {
+                    enable = false,
+                },
+            }
+        }
+    })
 
     -- Mason/LSP integration
     ----------------------------------------------------------------------------
     local mason_lspconfig = reload('mason-lspconfig')
 
     mason_lspconfig.setup({
-        -- Enable after Neovim 0.11.0
-        automatic_enable = true,
         -- A list of tools to automatically install if they're not already installed.
         ensure_installed = {
             'bashls',
@@ -201,6 +234,6 @@ return {
             'saghen/blink.cmp',
             'mason-lspconfig.nvim',
         },
-        config = nvim_lspconfig_config,
+        config = lsp_config,
     },
 }
