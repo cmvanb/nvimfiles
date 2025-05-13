42e0c93a59941bd8a173cafd50097e32a6211547
Casper <draakie@proton.me>
2025-05-13 22:02:08 +0200

    plugins: Minor cleanup.

--- lua/plugins/copilot_vim.lua
+++ lua/plugins/copilot_vim.lua
@@ -6,7 +6,7 @@
 local HighlightsUtils = reload('utils.highlights')
 local hi = HighlightsUtils.highlight
 
-hi('CopilotSuggestion',        { fg = 'text_6' },                                        { fg = 'ansi_brblack' })
+hi('CopilotSuggestion',  { fg = 'text_6' },  { fg = 'ansi_brblack' })
 
 -- Config
 return {
