f3b0357616f0b4cbd34ea8a1cea791e252e20403
Casper <draakie@proton.me>
2025-08-17 18:02:23 +0200

    diagnostics: Configure inline diag plugin.

--- lua/plugins/inline_diagnostic.lua
+++ lua/plugins/inline_diagnostic.lua
@@ -3,13 +3,30 @@
 --------------------------------------------------------------------------------
 
 local function config()
-    reload('tiny-inline-diagnostic').setup()
+    reload('tiny-inline-diagnostic').setup({
+        preset = 'powerline',
+        hi = {
+            mixing_color = 'None',
+        },
+        options = {
+            use_icons_from_diagnostic = true,
+            show_all_diags_on_cursorline = false,
+            multilines = {
+                enabled = false,
+                tabstop = 2,
+            },
+            overflow = {
+                mode = 'oneline',
+                padding = 0,
+            },
+        },
+    })
 end
 
 return {
     {
         'rachartier/tiny-inline-diagnostic.nvim',
-        event = 'VeryLazy',
+        event = 'LspAttach',
         priority = 1000,
         config = config,
     },
