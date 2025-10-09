86b465b7b134c6237d447d15c9f5d4794b605de5
Casper <draakie@proton.me>
2025-10-09 13:45:50 +0200

    [WIP] change matchup sign (not working)

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -3,11 +3,15 @@
 --------------------------------------------------------------------------------
 
 local function config()
-    reload('match-up').setup({
-        treesitter = {
-            stopline = 500
-        }
-    })
+    -- reload('match-up').setup({
+    --     treesitter = {
+    --         stopline = 500
+    --     }
+    -- })
+
+    require('mathch-up').setup({})
+
+    vim.g.matchup_matchparen_end_sign = '>'
 end
 
 return {
