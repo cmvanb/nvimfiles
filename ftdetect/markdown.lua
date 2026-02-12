4835eb2e3796845544db0c90a10d8434bea5792b
Casper <casper@codemu.ch>
2026-02-12 23:28:50 +0100

    markdown: Add keymap for preview-markdown script.

--- ftdetect/markdown.lua
+++ ftdetect/markdown.lua
@@ -1,9 +1,24 @@
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
     pattern = "*.md",
-    callback = function()
+    callback = function(args)
         vim.bo.filetype = 'markdown'
 
         -- Wrap long lines
         vim.opt.wrap = true
+
+        -- Preview markdown keymap
+        local keys = require('utils.keymaps')
+        local filename = vim.api.nvim_buf_get_name(args.buf)
+
+        keys.nnoremap(
+            '<leader>v',
+            function()
+                vim.fn.jobstart('preview-markdown.sh ' .. vim.fn.shellescape(filename))
+            end,
+            {
+                buffer = args.buf,
+                desc = 'Preview markdown'
+            }
+        )
     end
 })
