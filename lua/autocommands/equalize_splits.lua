c0eeef2fab6199c38dab0326527fc4e026f47ad7
Casper <casper@codemu.ch>
2026-03-17 17:50:09 +0100

    equalize-splits: Don't trigger on BufWipeout, was happening far too often.

--- lua/autocommands/equalize_splits.lua
+++ lua/autocommands/equalize_splits.lua
@@ -4,7 +4,7 @@
 
 local augroup = vim.api.nvim_create_augroup('EqualizeSplits', { clear = true })
 
-vim.api.nvim_create_autocmd({ 'VimEnter', 'VimResized', 'BufWipeout' }, {
+vim.api.nvim_create_autocmd({ 'VimEnter', 'VimResized' }, {
     group = augroup,
     callback = function()
         vim.cmd('wincmd =')
