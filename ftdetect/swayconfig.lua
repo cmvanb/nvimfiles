55a58461e682a5f0348ba1193b03c0068b092079
Casper <casper@codemu.ch>
2026-03-18 16:27:04 +0100

    ftdetect: Detect sway config more robustly.

--- ftdetect/swayconfig.lua
+++ ftdetect/swayconfig.lua
@@ -1,6 +1,19 @@
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-    pattern = '**/sway/*.conf',
-    callback = function()
+    -- Match config files anywhere under a sway/ or .sway/ directory tree.
+    -- Covers split configs (keymaps.conf, outputs.conf) and bare config files,
+    -- both at the deployed path (~/.config/sway/) and in dotfiles source trees
+    -- where sway/ may not be the direct parent (e.g. modules/sway/src/).
+    pattern = {
+        '**/{,.}sway/config',
+        '**/{,.}sway/**/config',
+        '**/{,.}sway/*.conf',
+        '**/{,.}sway/**/*.conf',
+    },
+    callback = function(args)
+        -- Require sway/ or .sway/ as an actual path component.
+        if not args.file:match('/%.?sway/') then return end
+        if args.file:match('sway%-portals%.conf$') then return end
+
         vim.bo.filetype = 'swayconfig'
     end
 })
