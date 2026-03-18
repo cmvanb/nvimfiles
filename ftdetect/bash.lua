2c7c0074c1d61cefa96a84343591a7effdecee2d
Casper <casper@codemu.ch>
2026-03-18 16:07:18 +0100

    chore: Remove unnecessary ftdetects for esh templates.

--- ftdetect/bash.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-    pattern = "*.sh~esh",
-    callback = function()
-        vim.bo.filetype = 'bash'
-    end
-})
--- ftdetect/fish.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-    pattern = "*.fish~esh",
-    callback = function()
-        vim.bo.filetype = 'fish'
-    end
-})
