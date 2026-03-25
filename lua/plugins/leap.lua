ac4a73b970df0d23a59902da576c525c08f7f9a5
Casper <casper@codemu.ch>
2026-03-25 23:30:27 +0100

    leap: Remove unused highlights.

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,16 +2,6 @@
 -- Leap configuration
 --------------------------------------------------------------------------------
 
--- Highlights
-local HighlightsUtils = reload('utils.highlights')
-local hi = HighlightsUtils.highlight
-
-hi('LeapMatch',           { fg = 'debug', attrs = 'bold' },                         { fg = 'ansi_magenta' })
-hi('LeapLabelPrimary',    { fg = 'black', bg = 'primary_15', attrs = 'bold' },     { fg = 'ansi_magenta' })
-hi('LeapLabelSecondary',  { fg = 'primary_2', bg = 'primary_11', attrs = 'bold' },  { fg = 'ansi_brmagenta' })
-hi('LeapLabelSelected',   { fg = 'debug', attrs = 'bold' },                         { fg = 'ansi_brmagenta' })
-
--- Config
 local function config()
     reload('leap').setup({
         case_sensitive = true,
