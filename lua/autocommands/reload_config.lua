e947939f99b39d09a024fcc950bec0e345b23613
Casper <casper@codemu.ch>
2026-04-28 16:52:01 +0200

    autocommands: Glob pattern for reload_config matches all lua files.

--- lua/autocommands/reload_config.lua
+++ lua/autocommands/reload_config.lua
@@ -7,7 +7,7 @@ local augroup = vim.api.nvim_create_augroup('ReloadConfig', { clear = true })
 
 vim.api.nvim_create_autocmd('BufWritePost', {
     group = augroup,
-    pattern = config_path .. '/*.lua',
+    pattern = config_path .. '/**/*.lua',
     callback = function()
         reload_config()
     end,
